7666069cb3503b14d8b7bcbfe2f1ccef4ae775d5
paulgrenning <paul.grenning@gmail.com>
2011-04-02 23:24:57 -0500

    started on the book page, changed the controller around to pass css ids dynamically to the header of each page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < ApplicationController
 
   def home
 	  @title = "Home"
-    @page_header = { :name => "James Grenning", :link => root_path}
+    @page_header = { :name => "James Grenning", :headerId => "header-name", :link => root_path}
+  end
+
+  def mybook
+    @title = "Test Driven Development For Embedded C"
+    @page_header = {:name => "Test-Driven Development For Embedded C", :headerId => "bookPage", :link => root_path}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,6 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
   map.home'/home', :controller => 'pages', :action => 'home'
+  map.mybook'/My-Book', :controller => 'pages', :action => 'mybook'
 
 
 
