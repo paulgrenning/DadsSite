dfb5e3f58b6065163a2c90d18012e80d8f089033
paulgrenning <paul.grenning@gmail.com>
2011-05-28 16:47:42 -0500

    added a reroute in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,19 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery # See ActionControl er::RequestForgeryProtection for detaile
+
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'myapp.mydomain.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
+
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
