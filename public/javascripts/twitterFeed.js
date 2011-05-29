2ce8746a6abe60a07ef2e22af941b35be0b6d29f
paulgrenning <paul.grenning@gmail.com>
2011-05-29 01:57:35 -0500

    dynamic twitter feed based on the lengthof the page

--- public/javascripts/twitterFeed.js
+++ public/javascripts/twitterFeed.js
@@ -1,17 +1,15 @@
-
-$.getJSON("http://twitter.com/statuses/user_timeline.json?screen_name=jwgrenning&count=7&callback=?",
-function(data){
-  var html = ""; 
-  $.each(data, function(i,item){
-    html += "<div class=\"topOfTweet\"></div>";
-    html += "<div class=\"tweet\" prepend-1 append-1><p>" + item.text + "</p></div>";  
-    html += "<div class=\"bottomOfTweet\"></div>";
-   /* ct = ct.replace(/http:\/\/\S+/g,  '<a href="$&" target="_blank">$&</a>');
-    ct = ct.replace(/\s(@)(\w+)/g,    ' @<a onclick="javascript:pageTracker._trackPageview('/outgoing/twitter.com/');" href="http://twitter.com/$2" target="_blank">$2</a>');
-    ct = ct.replace(/\s(#)(\w+)/g,    ' #<a onclick="javascript:pageTracker._trackPageview('/outgoing/search.twitter.com/search?q=%');" href="http://search.twitter.com/search?q=%23$2" target="_blank">$2</a>');
-    */
+$(document).ready(function() {
+  $.getJSON("http://twitter.com/statuses/user_timeline.json?screen_name=jwgrenning&count=30&callback=?",
+  function(data){
+    $.each(data, function(i,item){
+      if(parseInt($("#main-content").css('height')) - 50 > parseInt($("#sidebar").css('height'))){
+        var html = ""; 
+        html += "<div class=\"topOfTweet\"></div>";
+        html += "<div class=\"tweet\" prepend-1 append-1><p>" + item.text + "</p></div>";  
+        html += "<div class=\"bottomOfTweet\"></div>";
+        $("#twitter-feed").append(html);
+      }
+    });
   });
-  html += "</ul>"
-    $("#twitter-feed").append(html);
 });
 
