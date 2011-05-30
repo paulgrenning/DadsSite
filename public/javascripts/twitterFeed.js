dd8522570ffeaa087832f91da5fb68bc302e55a8
paulgrenning <paul.grenning@gmail.com>
2011-05-30 13:03:19 -0500

    changed the sidebar to float right instead of a floating div that covered everything

--- public/javascripts/twitterFeed.js
+++ public/javascripts/twitterFeed.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $.getJSON("http://twitter.com/statuses/user_timeline.json?screen_name=jwgrenning&count=30&callback=?",
   function(data){
     $.each(data, function(i,item){
-      if(parseInt($("#main-content").css('height')) - 50 > parseInt($("#sidebar").css('height'))){
+      if(parseInt($("#main-content").css('height')) - 50 > parseInt($("#twitter-area").css('height'))){
         var html = ""; 
         html += "<div class=\"topOfTweet\"></div>";
         html += "<div class=\"tweet\" prepend-1 append-1><p>" + item.text + "</p></div>";  
